53f87eca955298d883f594b2e8c125aee1d1d6c8
Oscar Pantoja <osolpantoja@gmail.com>
2026-03-11 08:41:25 -0500

    actualizando conexion a base de datos

--- config/database.php
+++ config/database.php
@@ -1,8 +1,29 @@
 <?php
-echo "hola mundo";
-$num1=20;
-$num2=10;
-$add= $num1+$num2;
-echo "<br>";
-echo "<b<the addition is </b>". $add;
+//local database configuration
+$LOCAL_HOST = 'localhost';//127.0.0.1
+$LOCAL_DBNAME= 'app_beta';
+$LOCAL_USERNAME='postgres';
+$LOCAL_PASSWORD= 'unicesmag';
+$LOCAL_PORT='5432';
+
+//superbase database configuration
+
+$SUPA_host='';
+$SUPA_DBNNAME='';
+$SUPA_USERNAME='';
+$SUPA_PASSWORD='';
+$SUPA_PORT='';
+
+$data_conection="
+host=$LOCALHOST
+dbname=$LOCAL_DBNAME
+user=$LOCAL_USERNAME
+paswword=$LOCAL_PASWWORD
+port=$LOCAL_PORT
+";
+
+$conn = pg_connect($data_conection);
+if (!$conn){
+
+}
 ?>
